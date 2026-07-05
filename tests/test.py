7c24a25f054063647728a3dbc34cedf3bc25573a
srivaarch <srivaarch@outlook.com>
2026-07-05 17:00:19 -0500

    Add greeks testing to test.py

--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,19 @@
 from rerisk.pricing.black_scholes import black_scholes
+from rerisk.pricing.greeks import delta, gamma, vega, theta, rho
+
+stock_price = 500
+strike_price = 500
+time = 1
+rate = 0.05
+vol = 0.5
 
 # Black-Scholes test 
-price = black_scholes(500, 500, 1, 0.05, 0.5)
-print("Option price: ", price)
+print("Option price: ", black_scholes(stock_price, strike_price, time, rate, vol))
+
+# Greeks test
+print("Delta: ", delta(stock_price, strike_price, time, rate, vol))
+print("Gamma: ", gamma(stock_price, strike_price, time, rate, vol))
+print("Vega: ", vega(stock_price, strike_price, time, rate, vol))
+print("Theta: ", theta(stock_price, strike_price, time, rate, vol))
+print("Rho: ", rho(stock_price, strike_price, time, rate, vol))
+
